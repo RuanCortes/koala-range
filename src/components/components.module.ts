ee154673d7411a179c530a08c05d323724fe7a54
Ruan Cortes <cortesruan@hotmail.com>
2019-03-20 22:50:16 -0300

    Limpar repositório front-end

--- src/components/components.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { RangeMapComponent } from './range-map/range-map';
-@NgModule({
-	declarations: [RangeMapComponent],
-	imports: [],
-	exports: [RangeMapComponent]
-})
-export class ComponentsModule {}
